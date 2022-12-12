ba4bf522bb19b515edeeca69751e387bcd86c2a3
MaksimBozhko <bochkomax2000@gmail.com>
2022-12-12 13:33:13 +0300

    change Dialogs (22 lessons)

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import s from './Dialogs.module.css'
+import {NavLink} from "react-router-dom";
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItem}>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/1' >Anton</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/2' >Sveta</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/3' >Sasha</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/4' >Aleksandr</NavLink>
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className="message">hello!</div>
+                <div className="message">how are you?</div>
+                <div className="message">I'm ok</div>
+            </div>
         </div>
     );
 };
